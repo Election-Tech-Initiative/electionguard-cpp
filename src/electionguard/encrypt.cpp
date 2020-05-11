2e1f523299e00ab828211206cb30076d891d3e0d
Matt Wilhelm <github@addressxception.com>
2020-05-11 10:23:48 -0400

    namespace source files

--- src/electionguard/encrypt.cpp
+++ src/electionguard/encrypt.cpp
@@ -1,20 +1,30 @@
 #include "electionguard/encrypt.hpp"
+
 #include <iostream>
 
-electionguard::EncryptionCompositor::EncryptionCompositor() {
-    std::cout<<__func__<<" : Creating EncryptionCompositor["<<this<<"]"<<std::endl;
-}
+namespace electionguard
+{
+    EncryptionCompositor::EncryptionCompositor()
+    {
+        std::cout << __func__ << " : Creating EncryptionCompositor[" << this
+                  << "]" << std::endl;
+    }
+    EncryptionCompositor::~EncryptionCompositor()
+    {
+        std::cout << __func__ << " : Destroying EncryptionCompositor[" << this
+                  << "]" << std::endl;
+    }
 
-electionguard::EncryptionCompositor::~EncryptionCompositor() {
-    std::cout<<__func__<<" : Destroying EncryptionCompositor["<<this<<"]"<<std::endl;
-}
+    int EncryptionCompositor::encrypt()
+    {
+        std::cout << __func__ << " : encrypting by instance " << std::endl;
+        return 9;
+    }
 
-int electionguard::EncryptionCompositor::encrypt() {
-    std::cout<<__func__<<" : encrypting by instance "<<std::endl;
-    return 9;
-}
+    int encrypt_ballot()
+    {
+        std::cout << __func__ << " : encrypting by function " << std::endl;
+        return 8;
+    }
 
-int electionguard::encrypt_ballot() {
-    std::cout<<__func__<<" : encrypting by function "<<std::endl;
-    return 8;
-}
+} // namespace electionguard
--- src/electionguard/hash.cpp
+++ src/electionguard/hash.cpp
@@ -2,8 +2,11 @@
 
 #include <iostream>
 
-int electionguard::hash_elems(string const &a)
+namespace electionguard
 {
-    std::cout << __func__ << " : hashing " << a << std::endl;
-    return 5;
-}
+    int hash_elems(string const &a)
+    {
+        std::cout << __func__ << " : hashing " << a << std::endl;
+        return 5;
+    }
+} // namespace electionguard
