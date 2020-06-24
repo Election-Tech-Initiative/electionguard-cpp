c34b82dd9963a2437c281fc308813af4c5dc9e81
Matt Wilhelm <github@addressxception.com>
2020-06-24 11:55:08 -0400

    add new bignum test (WIP)
    add the new bignum preview code

--- src/electionguard/encrypt.cpp
+++ src/electionguard/encrypt.cpp
@@ -2,7 +2,14 @@
 
 #include "hash.hpp"
 
+#include <assert.h>
 #include <iostream>
+
+uint64_t one[64] = {1};
+
+extern "C" {
+#include "../kremlin/Hacl_Bignum4096.h"
+}
 
 namespace electionguard
 {
@@ -23,6 +30,15 @@ namespace electionguard
         auto result = hash_elems("some string");
         if (result == 5) {
         }
+
+        uint64_t res[64] = {};
+
+        uint64_t carry = Hacl_Bignum4096_add(one, one, res);
+        if (carry > 0) {
+        }
+
+        assert(res[0] == 2);
+
         return 9;
     }
 
