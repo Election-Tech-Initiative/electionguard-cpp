c811b8080d2202c40c47292cf388393245923386
Matt Wilhelm <github@addressxception.com>
2020-08-24 13:25:56 -0400

    fix compiler warning

--- test/electionguard/test_encrypt.cpp
+++ test/electionguard/test_encrypt.cpp
@@ -6,7 +6,7 @@ TEST_CASE("Encrypt")
 {
     using namespace electionguard;
 
-    auto cpp_encrypter = new EncryptionMediator();
+    auto *cpp_encrypter = new EncryptionMediator();
 
     CHECK(cpp_encrypter->encrypt() == 9);
 
