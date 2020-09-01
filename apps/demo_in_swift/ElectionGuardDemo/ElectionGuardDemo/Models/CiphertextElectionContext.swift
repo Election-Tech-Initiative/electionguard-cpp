b437682a90ba6644717a063857cb2d1c75479aeb
Ed Snider <ed.snider@infernored.com>
2020-09-01 15:43:01 -0400

    added CiphertextElectionContext model and updated data service with a function to return a hardcoded (demo) election context

---
+++ apps/demo_in_swift/ElectionGuardDemo/ElectionGuardDemo/Models/CiphertextElectionContext.swift
@@ -0,0 +1,27 @@
+//
+//  CiphertextElectionContext.swift
+//  ElectionGuardDemo
+//
+//  Created by Ed Snider on 9/1/20.
+//  Copyright © 2020 Microsoft. All rights reserved.
+//
+
+import Foundation
+
+struct CiphertextElectionContext: Codable {
+    let numberOfGuardians: Int?
+    let quorum: Int?
+    let elgamalPublicKey: String?
+    let descriptionHash: String?
+    let cryptoBaseHash: String?
+    let cryptoExtendedBaseHash: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case numberOfGuardians = "number_of_guardians"
+        case quorum
+        case elgamalPublicKey = "elgamal_public_key"
+        case descriptionHash = "description_hash"
+        case cryptoBaseHash = "crypto_base_hash"
+        case cryptoExtendedBaseHash = "crypto_extended_base_hash"
+    }
+}
