88b3175086167794e2d7709d05962e40cb1e7c61
Matt Wilhelm <github@addressxception.com>
2020-09-24 13:19:35 -0400

    Feature/ballot tracker (#57)
    * Add new Hacl bignum code
    
    fix failing tests
    
    * add mod tests
    
    * suppress redundant and irrelevant clang-tidy checks
    
    * move C type casting to variant_cast
    
    * add explicit size invariants and common ModP elements
    
    * use size invariants
    
    * cleanup compiler warnings
    
    * implement mod operations
    
    * Rework/refactor group.cpp
    
    refactor components of group.cpp to support vector based interfaces, and static_cast when pointers are required for interfacing with hacl.  Move conversion functions into static methods on the relevant classes.
    
    * make element constructors use constants
    
    * Implement DisjunctiveChaum-Pedersen
    
    add tests to Hacl showing the memory footprint.  handle edge cases for Q values that are out of bounds when adding and subtracting.
    
    * Address PR feedback
    
    * use smart pointers
    
    migrate the pointer-based c++ interfaces to use smart pointers throughout.  Use the pimpl idiom for most data objects.  validate that memory management naively works across the C API boundary.  address some compiler warnings.
    
    * fix GCC compiler errors
    
    * Add Hacl v5 library
    
    * Add decrypt method
    
    * Disable Cpp sanitizers
    
    Fix buffer underflow
    
    * Fix GCC compiler errors
    
    * remove commented out code
    
    * Generate tracking codes

--- test/electionguard/test_tracker.cpp
+++ test/electionguard/test_tracker.cpp
@@ -40,4 +40,47 @@ TEST_CASE("Get rotating tracker hash rotates")
     CHECK(&rotatingHash1 != &rotatingHash2);
 }
 
-// todo: TEST_CASE("Hash to words converts to words")
+TEST_CASE("Hash to words converts to words")
+{
+    // Arrange
+    auto deviceHash = Tracker::getHashForDevice(1234UL, "some-location-string");
+    const auto &ballotHash = ONE_MOD_Q();
+    const auto &anotherBallotHash = TWO_MOD_Q();
+    uint64_t timestamp = 1000;
+    auto firstRotatingHash = Tracker::getRotatingTrackerHash(*deviceHash, timestamp, ballotHash);
+    auto secondRotatingHash =
+      Tracker::getRotatingTrackerHash(*deviceHash, timestamp, anotherBallotHash);
+
+    // Act
+    auto deviceWords = Tracker::hashToWords(*deviceHash);
+    auto firstRotatingWords = Tracker::hashToWords(*firstRotatingHash);
+    auto secondRotatingWords = Tracker::hashToWords(*secondRotatingHash);
+
+    // Assert
+    CHECK(deviceWords.empty() == false);
+    CHECK(deviceWords != firstRotatingWords);
+    CHECK(deviceWords != secondRotatingWords);
+    CHECK(firstRotatingWords != secondRotatingWords);
+}
+
+TEST_CASE("Hash to words converts to known words")
+{
+    // Arrange
+    const auto expectedHash = "325ab2622d35311db0320c9f3b421ee93017d16b9e4c7fef06704eda4fa5e30b";
+    const auto expectedWords =
+      "administrator-airfield-app-alcove-addition-adobe-administration-achiever-ape-administrator-"
+      "accent-ant-advent-advocate-acknowledgment-ascend-adjustment-account-area-alliance-answer-"
+      "age-ambience-aspect-absence-almanac-agenda-armpit-agent-antibody-arthur-accelerator";
+    const auto &deviceHash = ONE_MOD_Q();
+    const auto &ballotHash = TWO_MOD_Q();
+    uint64_t timestamp = 1000;
+    auto trackerHash = Tracker::getRotatingTrackerHash(deviceHash, timestamp, ballotHash);
+
+    // Act
+    auto trackerWords = Tracker::hashToWords(*trackerHash, "-");
+
+    // Assert
+    CHECK(trackerWords.empty() == false);
+    CHECK(trackerHash->toHex() == expectedHash);
+    CHECK(trackerWords == expectedWords);
+}
