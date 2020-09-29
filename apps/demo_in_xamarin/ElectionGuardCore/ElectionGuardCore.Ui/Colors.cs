929cffa5c98001f87d3ad6830d7097242d04c988
Jesse Douglas <jesse.douglas@infernored.com>
2020-09-29 16:44:35 -0400

    Style nav bar back button

---
+++ apps/demo_in_xamarin/ElectionGuardCore/ElectionGuardCore.Ui/Colors.cs
@@ -0,0 +1,8 @@
+﻿namespace ElectionGuardCore.Ui
+{
+    public static class Colors
+    {
+        public static readonly string DarkGreen = "#26423D";
+        public static readonly string LightGreen = "#217A68";
+    }
+}
