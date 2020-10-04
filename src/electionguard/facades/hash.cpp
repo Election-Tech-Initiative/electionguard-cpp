58649f818c34c3659f4c40c0b6a8b8b197485f80
Matt Wilhelm <github@addressxception.com>
2020-10-03 22:40:55 -0400

    rework hash

--- src/electionguard/facades/hash.cpp
+++ src/electionguard/facades/hash.cpp
@@ -1,41 +1,55 @@
 #include "electionguard/hash.hpp"
 
+#include "../log.hpp"
 #include "electionguard/group.hpp"
-#include "memory_cache.hpp"
 #include "variant_cast.hpp"
 
 extern "C" {
 #include "electionguard/hash.h"
 }
 
-#include "../log.hpp"
-
-using electionguard::Cache;
 using electionguard::ElementModQ;
 using electionguard::hash_elems;
+using electionguard::Log;
 using std::vector;
 
-// TODO: safe initialization
-static Cache<ElementModQ> cache_element_mod_q;
-
-eg_element_mod_q_t *eg_hash_elems_string(const char *a)
+eg_electionguard_status_t eg_hash_elems_string(const char *a, eg_element_mod_q_t **out_handle)
 {
-    auto hash = hash_elems(a);
-    auto *reference = cache_element_mod_q.retain(move(hash));
-    return AS_TYPE(eg_element_mod_q_t, reference);
+    try {
+        auto hash = hash_elems(a);
+
+        *out_handle = AS_TYPE(eg_element_mod_q_t, hash.release());
+        return ELECTIONGUARD_STATUS_SUCCESS;
+    } catch (const exception &e) {
+        Log::error(": eg_hash_elems_string", e);
+        return ELECTIONGUARD_STATUS_ERROR_BAD_ALLOC;
+    }
 }
 
-eg_element_mod_q_t *eg_hash_elems_strings(const char **a, int size)
+eg_electionguard_status_t eg_hash_elems_strings(const char **a, size_t in_length,
+                                                eg_element_mod_q_t **out_handle)
 {
-    vector<string> v(a, a + size);
-    auto hash = hash_elems(v);
-    auto *reference = cache_element_mod_q.retain(move(hash));
-    return AS_TYPE(eg_element_mod_q_t, reference);
+    try {
+        vector<string> v(a, a + in_length);
+        auto hash = hash_elems(v);
+
+        *out_handle = AS_TYPE(eg_element_mod_q_t, hash.release());
+        return ELECTIONGUARD_STATUS_SUCCESS;
+    } catch (const exception &e) {
+        Log::error(": eg_hash_elems_strings", e);
+        return ELECTIONGUARD_STATUS_ERROR_BAD_ALLOC;
+    }
 }
 
-eg_element_mod_q_t *eg_hash_elems_int(const uint64_t a)
+eg_electionguard_status_t eg_hash_elems_int(const uint64_t a, eg_element_mod_q_t **out_handle)
 {
-    auto hash = hash_elems(a);
-    auto *reference = cache_element_mod_q.retain(move(hash));
-    return AS_TYPE(eg_element_mod_q_t, reference);
+    try {
+        auto hash = hash_elems(a);
+
+        *out_handle = AS_TYPE(eg_element_mod_q_t, hash.release());
+        return ELECTIONGUARD_STATUS_SUCCESS;
+    } catch (const exception &e) {
+        Log::error(": eg_hash_elems_int", e);
+        return ELECTIONGUARD_STATUS_ERROR_BAD_ALLOC;
+    }
 }
--- test/electionguard/test_hash.c
+++ test/electionguard/test_hash.c
@@ -7,7 +7,10 @@ bool test_hash(void) { return test_hash_elems(); }
 
 bool test_hash_elems(void)
 {
-    eg_element_mod_q_t *string_hash = eg_hash_elems_string("test");
+    eg_element_mod_q_t *string_hash = NULL;
+    if (eg_hash_elems_string("test", &string_hash)) {
+        assert(false);
+    }
     uint64_t *string_hash_data = NULL;
     size_t string_hash_data_size;
     if (eg_element_mod_q_get_data(string_hash, &string_hash_data, &string_hash_data_size)) {
@@ -23,7 +26,10 @@ bool test_hash_elems(void)
     assert(string_hash_data[0] > 0);
 
     const char *strings[] = {"test", "strings"};
-    eg_element_mod_q_t *strings_hash = eg_hash_elems_strings(strings, 2);
+    eg_element_mod_q_t *strings_hash = NULL;
+    if (eg_hash_elems_strings(strings, 2, &strings_hash)) {
+        assert(false);
+    }
     uint64_t *strings_hash_data = NULL;
     size_t strings_hash_data_size;
     if (eg_element_mod_q_get_data(string_hash, &strings_hash_data, &strings_hash_data_size)) {
@@ -36,7 +42,10 @@ bool test_hash_elems(void)
     assert(strings_hash_data_size == 4);
     assert(strings_hash_data[0] > 0);
 
-    eg_element_mod_q_t *int_hash = eg_hash_elems_int(1234);
+    eg_element_mod_q_t *int_hash = NULL;
+    if (eg_hash_elems_int(1234, &int_hash)) {
+        assert(false);
+    }
     uint64_t *int_hash_data = NULL;
     size_t int_hash_data_size;
     if (eg_element_mod_q_get_data(string_hash, &int_hash_data, &int_hash_data_size)) {
@@ -48,6 +57,16 @@ bool test_hash_elems(void)
     assert(int_hash != 0);
     assert(int_hash_data_size == 4);
     assert(int_hash_data[0] > 0);
+
+    if (eg_element_mod_q_free(string_hash)) {
+        assert(false);
+    }
+    if (eg_element_mod_q_free(strings_hash)) {
+        assert(false);
+    }
+    if (eg_element_mod_q_free(int_hash)) {
+        assert(false);
+    }
 
     return true;
 }
