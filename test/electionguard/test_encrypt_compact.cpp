b0b657d495e35a6f6c9db036a349ed40b0e89efc
Matt Wilhelm <github@addressxception.com>
2021-03-26 17:05:48 -0400

    refactor compact test cases to use a fixture model

--- test/electionguard/test_encrypt_compact.cpp
+++ test/electionguard/test_encrypt_compact.cpp
@@ -11,56 +11,74 @@ using namespace electionguard;
 using namespace electionguard::test::mocks;
 using namespace std;
 
-TEST_CASE("Can encrypt compact ballots")
-{
-    auto keypair = ElGamalKeyPair::fromSecret(TWO_MOD_Q());
-    auto metadata = ElectionGenerator::getFakeMetadata(TWO_MOD_Q());
-    auto context = ElectionGenerator::getFakeContext(*metadata, *keypair->getPublicKey());
-    auto device = make_unique<EncryptionDevice>(12345UL, 23456UL, 34567UL, "Location");
-    auto mediator = make_unique<EncryptionMediator>(*metadata, *context, *device);
-    auto plaintext = BallotGenerator::getFakeBallot(*metadata);
+struct TestEncryptFixture {
 
-    // Act
-    auto compactCiphertext = mediator->compactEncrypt(*plaintext);
+    unique_ptr<ElGamalKeyPair> keypair;
+    unique_ptr<InternalElectionDescription> metadata;
+    unique_ptr<CiphertextElectionContext> context;
+    unique_ptr<EncryptionDevice> device;
+    unique_ptr<EncryptionMediator> mediator;
+    unique_ptr<PlaintextBallot> plaintext;
+    unique_ptr<CompactCiphertextBallot> compactCiphertext;
 
-    // Assert
-    CHECK(compactCiphertext->getObjectId() == plaintext->getObjectId());
-
-    SUBCASE("Can Serialize CompactCiphertextBallot")
+    TestEncryptFixture()
+        : keypair(ElGamalKeyPair::fromSecret(TWO_MOD_Q())),
+          metadata(ElectionGenerator::getFakeMetadata(TWO_MOD_Q())),
+          context(ElectionGenerator::getFakeContext(*metadata, *keypair->getPublicKey())),
+          device(make_unique<EncryptionDevice>(12345UL, 23456UL, 34567UL, "Location")),
+          mediator(make_unique<EncryptionMediator>(*metadata, *context, *device)),
+          plaintext(BallotGenerator::getFakeBallot(*metadata)),
+          compactCiphertext(mediator->compactEncrypt(*plaintext))
     {
-        auto json = compactCiphertext->toJson();
-        Log::debug(json);
+    }
+};
+
+TEST_SUITE("Test Encrypt Compact Ballot")
+{
+    auto fixture = make_unique<TestEncryptFixture>();
+
+    TEST_CASE("Can encrypt compact ballots")
+    {
         // Assert
-        auto msgpack = compactCiphertext->toMsgPack();
-        auto fromMsgpack = CompactCiphertextBallot::fromMsgPack(msgpack);
-        CHECK(fromMsgpack->getNonce()->toHex() == compactCiphertext->getNonce()->toHex());
+        CHECK(fixture->compactCiphertext->getObjectId() == fixture->plaintext->getObjectId());
     }
 
-    SUBCASE("Can Expand Plaintext")
+    TEST_CASE("Can Serialize CompactCiphertextBallot")
     {
-        auto expandedPlaintext =
-          expandCompactPlaintextBallot(*compactCiphertext->getPlaintext(), *metadata);
+        auto json = fixture->compactCiphertext->toJson();
+        Log::debug(json);
+        // Assert
+        auto msgpack = fixture->compactCiphertext->toMsgPack();
+        auto fromMsgpack = CompactCiphertextBallot::fromMsgPack(msgpack);
+        CHECK(fromMsgpack->getNonce()->toHex() == fixture->compactCiphertext->getNonce()->toHex());
+    }
+
+    TEST_CASE("Can Expand Plaintext")
+    {
+        auto expandedPlaintext = expandCompactPlaintextBallot(
+          *fixture->compactCiphertext->getPlaintext(), *fixture->metadata);
 
         // Assert
-        CHECK(expandedPlaintext->getObjectId() == compactCiphertext->getObjectId());
+        CHECK(expandedPlaintext->getObjectId() == fixture->compactCiphertext->getObjectId());
 
         auto msgpack = expandedPlaintext->toMsgPack();
         auto fromMsgPack = PlaintextBallot::fromMsgPack(msgpack);
         CHECK(fromMsgPack->getObjectId() == expandedPlaintext->getObjectId());
     }
 
-    SUBCASE("Can Expand Ciphertext")
+    TEST_CASE("Can Expand Ciphertext")
     {
-        auto json = compactCiphertext->toJson();
+        auto json = fixture->compactCiphertext->toJson();
         Log::debug(json);
 
-        auto expandedCiphertext =
-          expandCompactCiphertextBallot(*compactCiphertext, *metadata, *context);
+        auto expandedCiphertext = expandCompactCiphertextBallot(
+          *fixture->compactCiphertext, *fixture->metadata, *fixture->context);
 
         // Assert
-        CHECK(expandedCiphertext->getNonce()->toHex() == compactCiphertext->getNonce()->toHex());
-        CHECK(expandedCiphertext->isValidEncryption(*context->getDescriptionHash(),
-                                                    *keypair->getPublicKey(),
-                                                    *context->getCryptoExtendedBaseHash()) == true);
+        CHECK(expandedCiphertext->getNonce()->toHex() ==
+              fixture->compactCiphertext->getNonce()->toHex());
+        CHECK(expandedCiphertext->isValidEncryption(
+                *fixture->context->getDescriptionHash(), *fixture->keypair->getPublicKey(),
+                *fixture->context->getCryptoExtendedBaseHash()) == true);
     }
 }
