2e3442537d1501dc877a4117438036c26879adeb
Matt Wilhelm <github@addressxception.com>
2021-09-13 10:01:18 -0400

    Add benchmark project

---
+++ bindings/netstandard/ElectionGuard/ElectionGuard.Encryption.Bench/Program.cs
@@ -0,0 +1,13 @@
+﻿using ElectionGuard.Bench;
+using System;
+
+namespace ElectionGuard.Encryption.Bench
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchEncrypt.Bench_Encrypt_Ballot_Simple();
+        }
+    }
+}
