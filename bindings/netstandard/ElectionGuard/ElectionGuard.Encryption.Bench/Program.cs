a624717b5bd9f84780a43702204db756ca06a045
Matt Wilhelm <github@addressxception.com>
2021-10-20 15:37:26 -0400

    add bench, add methods to safehandle

--- bindings/netstandard/ElectionGuard/ElectionGuard.Encryption.Bench/Program.cs
+++ bindings/netstandard/ElectionGuard/ElectionGuard.Encryption.Bench/Program.cs
@@ -7,7 +7,14 @@ namespace ElectionGuard.Encryption.Bench
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("benchmarking");
+
             BenchEncrypt.Bench_Encrypt_Ballot_Simple();
+            BenchManifest.Bench_Can_Construct_Internationalized_Text();
+
+            Console.WriteLine("done! press any key to exit");
+
+            Console.ReadKey();
         }
     }
 }
