e7ee31470449af525d8ecd25bed5d49c394ef56e
Lee Richardson <github@leerichardson.com>
2022-06-01 13:03:51 -0400

    Encrypt plaintext and output to out dir

--- bindings/netstandard/ElectionGuard/ElectionGuard.Encryption.Cli/Encrypt/EncryptCommand.cs
+++ bindings/netstandard/ElectionGuard/ElectionGuard.Encryption.Cli/Encrypt/EncryptCommand.cs
@@ -20,18 +20,52 @@
         {
             encryptOptions.Validate();
 
-            var context = await File.ReadAllTextAsync(encryptOptions.Context);
-            Console.WriteLine(context);
+            var context = await GetContext(encryptOptions.Context);
             var enumerateFiles = Directory.EnumerateFiles(encryptOptions.BallotsDir);
+
+            var internalManifest = await GetInternalManifest(encryptOptions);
+            var device = GetDevice();
 
             foreach (var ballotFile in enumerateFiles)
             {
                 var ballot = await File.ReadAllTextAsync(ballotFile);
-                Console.WriteLine($"Parsing: {ballot}");
+                Console.WriteLine($"Parsing: {ballotFile}");
                 var plaintextBallot = new PlaintextBallot(ballot);
-                //ElectionGuard.Encrypt.Ballot(plaintextBallot, internalManifest);
+                var ciphertextBallot = ElectionGuard.Encrypt.Ballot(plaintextBallot, internalManifest, context, device.GetHash());
+                await WriteEncryptedBallot(encryptOptions, ballotFile, ciphertextBallot);
             }
             Console.WriteLine("Parsing Complete");
+        }
+
+        private static async Task WriteEncryptedBallot(EncryptOptions encryptOptions, string ballotFile,
+            CiphertextBallot ciphertextBallot)
+        {
+            var ballotJson = ciphertextBallot.ToJson();
+            var outFile = Path.Join(encryptOptions.OutDir, Path.GetFileName(ballotFile));
+            await File.WriteAllTextAsync(outFile, ballotJson);
+        }
+
+        private static ElementModQ GetNonce()
+        {
+            return new ElementModQ("4BD3231DC17E9E84F5B0A5D2C4160C6A2299EDAE184C291E17709913B8F9CB40");
+        }
+
+        private static EncryptionDevice GetDevice()
+        {
+            return new EncryptionDevice(12345UL, 23456UL, 34567UL, "Location");
+        }
+
+        private static async Task<InternalManifest> GetInternalManifest(EncryptOptions encryptOptions)
+        {
+            var manifestJson = await File.ReadAllTextAsync(encryptOptions.Manifest);
+            var manifest = new Manifest(manifestJson);
+            return new InternalManifest(manifest);
+        }
+
+        private static async Task<CiphertextElectionContext> GetContext(string contextFile)
+        {
+            var contextJson = await File.ReadAllTextAsync(contextFile);
+            return new CiphertextElectionContext(contextJson);
         }
     }
 }
--- bindings/netstandard/ElectionGuard/ElectionGuard.Encryption.Cli/Encrypt/EncryptOptions.cs
+++ bindings/netstandard/ElectionGuard/ElectionGuard.Encryption.Cli/Encrypt/EncryptOptions.cs
@@ -8,19 +8,38 @@ internal class EncryptOptions
     [Option('c', "context", Required = true, HelpText = "Json file containing an ElectionGuard context that contains encryption details.")]
     public string Context { get; set; }
 
+    [Option('m', "manifest", Required = true, HelpText = "Json file containing an ElectionGuard manifest that contains election details.")]
+    public string Manifest { get; set; }
+
     [Option('b', "ballots", Required = true, HelpText = "File folder containing ballots to encrypt.")]
     public string BallotsDir { get; set; }
 
+    [Option('o', "out", Required = true, HelpText = "File folder in which to place encrypted ballots.")]
+    public string OutDir { get; set; }
+
     public void Validate()
     {
-        if (!Directory.Exists(BallotsDir))
-        {
-            throw new ArgumentException($"{BallotsDir} does not exist");
-        }
+        ValidateFiles();
+        ValidateDirectories();
+    }
 
-        if (!File.Exists(Context))
+    private void ValidateDirectories()
+    {
+        var requiredDirectories = new[] { BallotsDir, OutDir };
+        var missingDirectories = requiredDirectories.Where(f => !Directory.Exists(f));
+        foreach (var directory in missingDirectories)
         {
-            throw new ArgumentException($"{Context} does not exist");
+            throw new ArgumentException($"{directory} does not exist");
+        }
+    }
+
+    private void ValidateFiles()
+    {
+        var requiredFiles = new[] { Context, Manifest };
+        var missingFiles = requiredFiles.Where(f => !File.Exists(f));
+        foreach (var file in missingFiles)
+        {
+            throw new ArgumentException($"{file} does not exist");
         }
     }
 }
